5bded597c729a98293b47de4d928d73f189db51c
vijay-varma64 <157892362+vijay-varma64@users.noreply.github.com>
2025-03-30 20:35:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "web Developer", "UI/UX Designer", "Freelancer"],
+    typeSpeed: 180,
+    BackSpeed: 60,
+    loop: true
+})
+var typed = new Typed(".typingg", {
+    strings: ["", "Web Designer", "web Developer", "UI/UX Designer", "Freelancer"],
+    typeSpeed: 60,
+    BackSpeed: 60,
+    loop: true
+})
